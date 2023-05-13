e1b1bafa41efcdffe50a4d4dd6a91c9651d329b3
Kononirin <kononirin@gmail.com>
2023-05-13 10:23:23 +0500

    Initial commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    WebDriver driver = new ChromeDriver();
+
+    @BeforeMethod
+    protected void beforeMethod() {
+        driver.get("http://localhost:8080/");
+
+        driver.findElement(By.name("j_username")).sendKeys("kononirin");
+        driver.findElement(By.name("j_password")).sendKeys("nthvbyfnjh123");
+    }
+
+    @AfterMethod
+    protected void afterMethod() {
+        driver.quit();
+    }
+}
